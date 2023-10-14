25cd7a7678c0abd9440d6229e480af6d3d646721
UmerFruit <umerfarooq1105@gmail.com>
2023-10-14 12:26:24 +0500

    .

--- Testing.cpp
+++ Testing.cpp
@@ -1,5 +1,40 @@
 #include "Headers.h"
+#include <queue>
+void part1(string name);
+void part2(string name);
+void part3(string name);
 int main()
 {
-    
+    cout << "\ta" << endl;
+    cout << "        a" << endl;
+}
+void part3(string name)
+{
+    queue<char> q;
+    for (int i = 0; i < name.length(); i++)
+    {
+        q.push(name[i]);
+    }
+}
+void part2(string name)
+{
+    string temp;
+    for (int j = 0; j < 4; j++)
+    {
+        for (int i = 1; i <= 26; i++)
+        {
+            temp = name;
+            for (int z = 0; z < temp.length(); z++)
+            {
+                temp[z] += rand() % i;
+            }
+            cout << temp << flush << '\r';
+            usleep(60000);
+        }
+    }
+    cout << name;
+}
+
+void part1(string name)
+{
 }
--- lol.c
+++ lol.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
-void main()
-{
-    int n1;
-    int n2;
-    int n3;
-    int q = n1/n2;
-    int r = n1 - (n2*q);
-    printf("%i\n",r);
+#include <unistd.h>
+
+int main() {
+    char name[] = "umerfarooq";
+    char c;
+
+    for (int j = 0; j < sizeof(name) - 1; j++) {
+        for (int i = 0; i < 26; i++) {
+            c = 'a' + i;
+            usleep(30000);
+            printf("%c", c);
+            //bas ye wali ek hevy command hai baqi to tujhe ata hai
+            fflush(stdout);          
+            
+            printf("\b");   
+            fflush(stdout);
+        }
+
+        for (int i = 0; i < 26; i++) {
+            c = 'a' + i;
+            usleep(30000);
+            printf("%c", c);
+            fflush(stdout);     
+            if (c != name[j]) {
+                printf("\b");
+                fflush(stdout);
+            } else
+                break;
+        }
+    }
+
+    return 0;
 }
