7a4bd8bc183df5c841084759fb51af6ae6218180
UmerFruit <umerfarooq1105@gmail.com>
2023-11-26 19:11:33 +0500

    .

--- TestingWorkspace/Demo_Marks.cpp
+++ TestingWorkspace/Demo_Marks.cpp
@@ -1,8 +1,8 @@
 #include "Headers.h"
 using namespace std;
-void Bass5()
+void ass5(string section)
 {
-    fstream csv("BMarks5.csv", ios::out | ios::app);
+    fstream csv(section + "Marks5.csv", ios::out | ios::app);
     char ch;
     string rollnum, name;
     int marks;
@@ -40,10 +40,11 @@ void Bass5()
         cout << "Add new Record? (y/n)" << endl;
         cin >> ch;
     } while (ch != 'n');
+    csv.close();
 }
-void Bass6()
+void ass6(string section)
 {
-    fstream csv("BMarks6.csv", ios::out | ios::app);
+    fstream csv(section + "Marks6.csv", ios::out | ios::app);
     char ch;
     string rollnum, name;
     int marks;
@@ -80,89 +81,9 @@ void Bass6()
         cout << "Add new Record? (y/n)" << endl;
         cin >> ch;
     } while (ch != 'n');
+    csv.close();
 }
-void Aass5()
-{
-    fstream csv("AMarks5.csv", ios::out | ios::app);
-    char ch;
-    string rollnum, name;
-    int marks;
-    bool cmt;
-    csv << endl;
-    do
-    {
-        cout << "Enter name: ";
-        cin >> name;
-        csv << name << ",";
-        cout << "Roll Number: ";
-        cin >> rollnum;
-        csv << rollnum << ",";
-
-        for (int i = 1; i <= 8; i++)
-        {
-            cout << "Marks of Q" << i;
-            if (i == 4 || i == 3)
-                cout << " (Out of 20)";
-            cout << ": ";
-            cin >> marks;
-            csv << marks;
-            csv << ',';
-        }
-        cout << "Enter Comment(0/1)";
-        cin >> cmt;
-        if (cmt)
-        {
-            string com;
-            cin.ignore();
-            getline(cin, com);
-            csv << com;
-        }
-        csv << endl;
-        cout << "Add new Record? (y/n)" << endl;
-        cin >> ch;
-    } while (ch != 'n');
-}
-void Aass6()
-{
-    fstream csv("AMarks6.csv", ios::out | ios::app);
-    char ch;
-    string rollnum, name;
-    int marks;
-    bool cmt;
-    csv << endl;
-    do
-    {
-        cout << "Enter name: ";
-        cin >> name;
-        csv << name << ",";
-        cout << "Roll Number: ";
-        cin >> rollnum;
-        csv << rollnum << ",";
-
-        for (int i = 1; i <= 4; i++)
-        {
-            cout << "Marks of Q" << i;
-            cout << "(out of 25)";
-            cout << ": ";
-            cin >> marks;
-            csv << marks;
-            csv << ',';
-        }
-        cout << "Enter Comment(0/1)";
-        cin >> cmt;
-        if (cmt)
-        {
-            string com;
-            cin.ignore();
-            getline(cin, com);
-            csv << com;
-        }
-        csv << endl;
-        cout << "Add new Record? (y/n)" << endl;
-        cin >> ch;
-    } while (ch != 'n');
-}
-void Asec()
+void sec(string section)
 {
     char c;
     char cont;
@@ -173,58 +94,29 @@ void Asec()
 
         if (c == '5')
         {
-            Aass5();
+            ass5(section);
         }
         if (c == '6')
         {
-            Aass6();
+            ass6(section);
         }
         cout << "new assignment? (y/n)" << endl;
         cin >> cont;
     } while (cont == 'y');
 }
-void Bsec()
-{
-    char cont;
-    char c;
-    do
-    {
-        cout << "Assignment number: ";
-        cin >> c;
-
-        if (c == '5')
-        {
-            Bass5();
-        }
-        if (c == '6')
-        {
-            Bass6();
-        }
-        cout << "new assignment? (y/n)" << endl;
-        cin >> cont;
-    } while (cont == 'y');
-
-}
-
 int main()
 {
-    char sec;
+    string section;
     char cont;
-
     do
     {
         cout << "Section? (A/B)";
-        cin >> sec;
-        if(sec == 'A' || sec == 'a')
-        {
-            Asec();
-        }
-        if (sec == 'B' || sec == 'b')
-        {
-           Bsec();
-        }
+        cin >> section;
+        section.push_back(static_cast<char>(toupper(section[0])));
+        section.erase(section.begin());
+        sec(section);
         cout << "Section Change? (y/n)" << endl;
-        cin>>cont;
+        cin >> cont;
     } while (cont == 'y');
     return 0;
 }
