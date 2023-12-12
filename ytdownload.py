83c0dee29c2ac5c098b60605025bbecea8c287bf
UmerFruit <umerfarooq1105@gmail.com>
2023-12-12 11:04:53 +0500

    .

--- ytdownload.py
+++ ytdownload.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
-
+import ssl
+ssl._create_default_https_context = ssl._create_stdlib_context
 try:
     # Ask the user to input the YouTube URL
     url = input("Enter the YouTube URL: ")
@@ -10,10 +11,11 @@ try:
     print("Views:", yt.views)
 
     # Get the highest resolution stream
-    yd = yt.streams.get_highest_resolution()
+    #yd = yt.streams.get_highest_resolution()
+    yt.streams.filter(res="1080p").first().download()
     
     # Download the video to the current directory
-    yd.download()
+   
     
     print("Download complete.")
 except Exception as e:
