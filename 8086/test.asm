925befab0237013112ea6eae9e9994593e0f0c63
UmerFruit <umerfarooq1105@gmail.com>
2024-02-23 10:20:29 +0500

    .

---
+++ 8086/test.asm
@@ -0,0 +1,25 @@
+.model small
+.stack 100h
+.data
+
+
+namecap db "UMER FAROOQ$"
+namesmall db "umer farooq$"
+IDword word 206h
+IDdword dword 206h
+IDbinword word 01000000110b
+IDbindword dword 01000000110b
+IDdecword word 518
+IDdecdword dword 518
+agebin byte 010100b
+agedec byte 20
+agebin byte 010100b
+cnic qword 051F66C2790h
+
+
+.code
+main proc
+
+
+main endp
+    end main
--- 8086/testing.asm
+++ 8086/testing.asm
@@ -1,23 +1,20 @@
 .model small
 .stack 100h
 .data
-    v1 db 15
-    v2 db 25
+v1 db 01h,25h,85h,21h,90h,1Ah,0Dh,0ffh,0Eh,0CCh
 .code
 main proc
 
     mov ax,@data
     mov ds,ax
-
-    mov ax,2
-    add ax,97
-
-    mov dx,ax
-
-    mov ah,2
-    int 21h
+    mov ax,word ptr [v1+2]
+    mov eax,dd ptr [v1+8]
+    mov eax,dd ptr [v1+5]
+    mov ax,word ptr [v1+9]
+    mov ax,db ptr v1
 
     mov ah,4ch
     int 21h
+
 main endp
     end main
