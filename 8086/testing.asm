61b6a265e958d067574e0a590db450651c0727c8
UmerFruit <umerfarooq1105@gmail.com>
2024-03-05 19:04:21 +0500

    .

--- 8086/testing.asm
+++ 8086/testing.asm
@@ -1,21 +1,26 @@
 .model small
 .stack 100h
 .data
+
 arr1 db 1,2,3,4,5
 warr2 dw 5 dup(25)
-arr3 dd 25 dup(2024h)
-types db 3 dup(?)
-lengths db 3 dup(?)
-sizes db 3 dup(?)
-
+arr3 dd 2 dup(2024h)
+t db ?
+l db ?
+s db ?
 .code
 main proc
+
     mov ax,@data
     mov ds,ax
+    mov t, type arr1
+    mov l,length warr2
+    mov s, size arr3
 
-    mov types[0],type arr1
-    mov ah,4ch
+    mov si,offset arr3
     
+   
+    mov ah,4ch 
     int 21h
 
 main endp
