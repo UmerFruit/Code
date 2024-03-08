b8213fb1efce189a1a37b24bb34792c830b397c7
UmerFruit <umerfarooq1105@gmail.com>
2024-03-08 11:18:45 +0500

    .

--- testing.cpp
+++ testing.cpp
@@ -1,29 +1,64 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include <iostream>
-#include <stdio.h>
+#include <vector>
+#include <cstring>
 using namespace std;
-// Defining an enumeration type called Color
-enum Color {
-    Red,
-    Green,
-    Blue
-};
 
-int main() {
-    // Declaring a variable of type Color and initializing it with a value from the enumeration
-    Color c = Red;
+int main()
+{
+    int p1[2]; // C => P
+    int p2[2]; // P => C
+    if (pipe(p1) == -1)
+        return 1;
+    if (pipe(p2) == -1)
+        return 1;
+    pid_t pid = fork();
+    if (pid == -1)
+        return 2;
+    else if (pid == 0)
+    {
+        close(p1[0]);
+        close(p2[1]);
 
-    // Switch statement to perform different actions based on the value of color
-    switch (c) {
-        case Color::Red:
-            cout << "The color is Red." << endl;
-            cout<<Bluew<<endl;;
-            break;
-        case Color::Green:
-            cout << "The color is Green." << endl;
-            break;
-        case Color::Blue:
-            cout << "The color is Blue." << endl;
-            break;
+        string x;
+        read(p2[0], &x, sizeof(x));
+        cout << "Child: " << x << endl;
+        vector<string> v= {"umer", "ali", "ahmed", "khan", "Jamaal", "maleek", "saif", "humayun", "arsalan", "ash"};
+        for (int i = 0; i < 10; i++)
+        {
+            write(p1[1], &v[i], sizeof(string));
+        }
+        
+
+        close(p2[0]);
+        close(p1[1]);
+    }
+    else
+    {
+        close(p2[0]);
+        close(p1[1]);
+
+        string y = "Hello umer";
+        write(p2[1], &y, sizeof(y));
+        cout << "Wrote from Parent: " << y << endl;
+        vector<string> rec(10);
+        for (int i = 0; i < 10; i++)
+        {
+            read(p1[0], &rec[i], sizeof(string));
+        }
+        
+        
+        cout << "Parent: " << y << endl;
+        for (int i = 0; i < 10; i++)
+        {
+            cout << rec[i] << " ";
+        }
+        cout << endl;
+
+        close(p2[1]);
+        close(p1[0]);
     }
 
     return 0;
