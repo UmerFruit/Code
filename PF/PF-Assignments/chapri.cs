a688a73610f60ac01c6377306bc545d7fee7056e
UmerFruit <umerfarooq1105@gmail.com>
2024-05-20 02:59:07 -0400

    bismillah

---
+++ PF/PF-Assignments/chapri.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Your code goes here
+
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
